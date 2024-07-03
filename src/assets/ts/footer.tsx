6c9cbd2b8bc336570e08cc64c6cf6fd04dae49b9
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-03 10:19:11 -0700

    5-style/feat. Added more React and SCSS elements
    Co-authored-by: Nicholas Nguyen <nonkulos@users.noreply.github.com>

---
+++ src/assets/ts/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../scss/footer.scss';
+
+const Footer: React.FC = (): React.JSX.Element => {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -10,24 +10,32 @@ const HomePage: React.FC = (): React.JSX.Element => {
             <Navbar />
             <div id="banner">
                 <p id="banner_title">Global Youth Scholars</p>
+
                 <p id="banner_desc">Welcome to the Global Youth Scholars homepage!</p>
             </div>
+
             <div id="homeabout">
                 <p id="about_title">About</p>
+
                 <div id="about_content">
                     <img src={img} alt="logo" />
                     <p id="about_text">Global Youth Scholars is a non-profit organization that aims to provide educational resources to underprivileged students around the world. We believe that education is a fundamental human right and that every child should have access to quality education. Our mission is to empower students to reach their full potential and become leaders in their communities. We offer a variety of programs and services to support students in their academic journey, including tutoring, mentorship, and scholarship opportunities. Join us in our mission to create a brighter future for all children!</p>
                 </div>
             </div>
+
             <div id="homeinfo">
                 <p id="info_title">Info</p>
-                <p id="info_content">nomnom content nyehehehe</p>
+
+                <div id="container">
+                    <p id="info_content">Global Youth Scholars is a non-profit organization that aims to provide educational resources to underprivileged students around the world. We believe that education is a fundamental human right and that every child should have access to quality education. Our mission is to empower students to reach their full potential and become leaders in their communities. We offer a variety of programs and services to support students in their academic journey, including tutoring, mentorship, and scholarship opportunities. Find out more about our programs and how you can get involved!</p>
+                    <a href="/about" id="info_link">Learn More</a>
+                </div>
             </div>
+
             <div id="homecontact">
                 <h2>Contact</h2>
                 <p>nomnom content nyehehehe</p>
             </div>
-
         </section>
     )
 }
