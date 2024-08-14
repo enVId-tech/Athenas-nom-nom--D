9bb0b06c5d5603d4de8e18a648fa755dedf0469d
Erick Tran <thelittlebotengineer@gmail.com>
2024-08-13 23:05:00 -0700

    feat: worked on publications and homepage
    added stuff to publications and worked on part of homepage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,14 @@ import styles from "@/styles/about.module.scss";
 export default function AboutPage(): React.JSX.Element {
 	return (
 		<section className={styles.aboutMain}>
-			<h1>About</h1>
-			<p>Welcome to the Global Youth Scholars program!</p>
+			<h1>About our organisation</h1>
+			<div className={styles.aboutTitle}>
+				
+			<div className={styles.aboutContent}>
+				<p>Welcome to the Global Youth Scholars program!</p>
+				<p>Our program offers students the opportunity to conduct research in various fields</p>
+			</div>
+			</div>
 		</section>
 	);
 }
--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -4,27 +4,28 @@ import styles from "@/styles/publications.module.scss";
 export default async function PublicationsPage(): Promise<React.JSX.Element> {
 	return (
 		<section className={styles.publicationsMain}>
-			<h1>Publications</h1>
-			<p>Welcome to the Global Youth Scholars program!</p>
-			<div className={styles.publicationsContent}>
-				<p>Here are some publications by our students:</p>
-				<ul>
-					<li>
-						<a href="https://example.com/publication1" target="_blank" rel="noreferrer noopener">
-							Publication 1
-						</a>
-					</li>
-					<li>
-						<a href="https://example.com/publication2" target="_blank" rel="noreferrer noopener">
-							Publication 2
-						</a>
-					</li>
-					<li>
-						<a href="https://example.com/publication3" target="_blank" rel="noreferrer noopener">
-							Publication 3
-						</a>
-					</li>
-				</ul>
+			<div className={styles.publicationsSearchbar}>
+				<input type="text" placeholder="Search for publications..." />
+				<button>Search</button>
+			</div>
+			<div className={styles.publicationsBody}>
+				<div className={styles.publicationsLeft}>
+					<div className={styles.publication}>
+						<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Publications" width={300} height={200} />
+						<div className={styles.publicationContent}>
+							<h2>Placeholder Publication</h2>
+							<p>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</p>
+						</div>
+					</div>
+				</div>
+				<div className={styles.publicationsRight}>
+					<div className={styles.publication}>
+						<h2 className={styles.name}>Publication 1</h2>
+						<p className={styles.authors}>Author 1, Author 2, Author 3</p>
+						<p className={styles.location}>Published in Journal 1</p>
+						<p className={styles.description}>Description of publication 1.</p>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
