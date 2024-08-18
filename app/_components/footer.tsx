41c72308fb76e2e66458922ef2ba9b42bcf0569c
Erick Tran <thelittlebotengineer@gmail.com>
2024-08-17 22:58:04 -0700

    feat: modified about page and footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,13 +1,33 @@
+// "use client";
 import React from "react";
-import Link from "next/link";
 import styles from "@/styles/_components/footer.module.scss";
 
-export default async function Footer(): Promise<React.JSX.Element> {
+export default function Footer(): React.JSX.Element {
+	// const [isVisible, setIsVisible] = React.useState<boolean>(false);
+
+	// const handleScroll = (): void => {
+	// 	const scrollY: number = window.scrollY;
+	// 	const windowHeight: number = window.innerHeight;
+	// 	const element: HTMLElement | null = document.getElementsByClassName(styles.footerMain)[0] as HTMLElement;
+
+	// 	if (element && scrollY + windowHeight >= element.offsetTop) {
+	// 		setIsVisible(true);
+	// 	}
+	// }
+
+	// React.useEffect((): (() => void) => {
+	// 	window.addEventListener("scroll", handleScroll);
+
+	// 	return () => {
+	// 		window.removeEventListener("scroll", handleScroll);
+	// 	}
+	// }, []);
+
 	return (
-		<p className={styles.footerMain}>
+		<p className={`${styles.footerMain}`}> {/*${isVisible ? styles.footerVisible : ""}*/}
 			Developed by Erick Tran (
-				<a href="https://github.com/envid-tech" target='_blank' rel="noreferrer noopener">enVIdTech</a>
-			) for the <Link href="/">Global Youth Scholars</Link> program.
+			<a href="https://github.com/envid-tech" target='_blank' rel="noreferrer noopener">enVIdTech</a>
+			) for the <a href="/" target='_blank' rel="noreferrer noopener">Global Youth Scholars</a> program.
 		</p>
 	)
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,56 +1,61 @@
 import React from "react";
 import styles from "@/styles/about.module.scss";
+import Footer from "../_components/footer";
 
 export default function AboutPage(): React.JSX.Element {
 	return (
 		<section className={styles.aboutMain}>
-			<h1>About our organisation</h1>
-			<div className={styles.aboutTitle}>
-				<div className={styles.aboutContent}>
-					<p>Created in 2024, our program is designed to help students do research in a variety of fields, including science, technology, engineering, and mathematics (STEM).</p>
-				</div>
-			</div>
-			<div className={styles.whereWeWork}>
-				<h2>Location</h2>
-				<div className={styles.whereWeWorkMain}>
-					<div className={styles.whereWeWorkText}>
-						<p>Our program is based in</p>
-						<h1>California, USA</h1>
-						<p>However, we have students from all over the world</p>
+			<div className={styles.aboutSubMain}>
+				<h1>About our organisation</h1>
+				<div className={styles.aboutTitle}>
+					<div className={styles.aboutContent}>
+						<p>Created in 2024, our program is designed to help students do research in a variety of fields, including science, technology, engineering, and mathematics (STEM).</p>
 					</div>
-					<img alt="California" src="https://chimicles.com/wp-content/uploads/2016/05/California-state-large.png" />
 				</div>
-			</div>
+				<div className={styles.whereWeWork}>
+					<h2>Location</h2>
+					<div className={styles.whereWeWorkMain}>
+						<div className={styles.whereWeWorkText}>
+							<p>Our program is based in</p>
+							<h1>California, USA</h1>
+							<p>However, we have students from all over the world</p>
+						</div>
+						<img alt="California" src="https://chimicles.com/wp-content/uploads/2016/05/California-state-large.png" />
+					</div>
+				</div>
 
-			<div className={styles.meetTheTeam}>
-				<h2>Meet the team</h2>
-				<div className={styles.teamMembers}>
-					<div className={styles.teamMember}>
-						<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
-						<h3>John Doe</h3>
-						<p>Director</p>
-						<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
-					</div>
-					<div className={styles.teamMember}>
-						<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
-						<h3>Jane Doe</h3>
-						<p>Researcher</p>
-						<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
-					</div>
-					<div className={styles.teamMember}>
-						<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
-						<h3>John Doe</h3>
-						<p>Director</p>
-						<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
-					</div>
-					<div className={styles.teamMember}>
-						<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
-						<h3>Jane Doe</h3>
-						<p>Researcher</p>
-						<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
+				<div className={styles.meetTheTeam}>
+					<h2>Meet the team</h2>
+					<div className={styles.teamMembers}>
+						<div className={styles.teamMember}>
+							<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
+							<h3>John Doe</h3>
+							<p>Director</p>
+							<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
+						</div>
+						<div className={styles.teamMember}>
+							<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
+							<h3>Jane Doe</h3>
+							<p>Researcher</p>
+							<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
+						</div>
+						<div className={styles.teamMember}>
+							<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
+							<h3>John Doe</h3>
+							<p>Director</p>
+							<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
+						</div>
+						<div className={styles.teamMember}>
+							<img src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png" alt="Team member" width={100} height={100} />
+							<h3>Jane Doe</h3>
+							<p>Researcher</p>
+							<h2>Ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc.</h2>
+						</div>
 					</div>
 				</div>
 			</div>
+			
+			<Footer />
 		</section>
 	);
 }
